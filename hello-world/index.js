1c97c3c185dcb042b77c10e70c1a62565a4f9a4e
Leejjon <leejjon@gmail.com>
2021-06-26 01:25:57 +0200

    Finished writing the blog.

--- hello-world/index.js
+++ hello-world/index.js
@@ -3,7 +3,7 @@ let http = require('http');
 
 http.createServer((req, res) => {
     console.log('url:', req.url);
-    res.end('hello world');
+    res.end('hello world from instance 1');
 }).listen(port, (error) => {
     console.log(`server is running on ${port}`);
 });
